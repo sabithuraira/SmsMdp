a154c35366e163583ec8cd22c255b9f35c751882
Sabit Huraira <sabitzhabit@gmail.com>
2017-11-07 11:36:08 +0700

    send sms from berita

--- app/models/berita.rb
+++ app/models/berita.rb
@@ -1,2 +1,16 @@
 class Berita < ApplicationRecord
+    after_save :sync_sms
+    
+    private
+        def sync_sms
+            if is_sms
+                Mahasiswa.all.each do |data|
+                    sms = Outbox.new
+                    sms.DestinationNumber= data.parent_phone
+                    sms.TextDecoded= informasi
+                    sms.CreatorID= "Gammu"
+                    sms.save
+                end
+            end
+        end
 end
